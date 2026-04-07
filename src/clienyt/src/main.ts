2bbf545fdfefcce5cfe39261f5c3448ab4b3cd8d
prakasg reddy kadira <kp.reddy.999@gmail.com>
2026-04-07 11:11:44 -0400

    ddd

--- src/clienyt/src/main.ts
+++ src/clienyt/src/main.ts
@@ -1,9 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
 document.addEventListener('DOMContentLoaded', () => {
-    platformBrowserDynamic().bootstrapModule(AppModule).catch((err) => {
+    bootstrapApplication(AppComponent, appConfig).catch((err) => {
         return console.error(err);
     });
 });
